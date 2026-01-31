12db383ee0ea92fb7a0f7d2069a3874bfef6897b
8a-ma <e0as0k98x001b@gmail.com>
2026-01-30 21:30:41 -0300

    refacto: redefine tables on database

--- db/build/init.sql
+++ db/build/init.sql
@@ -1,54 +1,41 @@
 -- db/init.sql
 
--- 1. Tabla de Usuarios
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- 2. Tabla de Categorías
--- El CHECK asegura que el tipo sea solo uno de los permitidos
+-- 1. Tabla de Categorías
+-- Define los tipos de categorias a los que los gastos y presupuestos estan asignados
 CREATE TABLE IF NOT EXISTS categories (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    tipo VARCHAR(20) NOT NULL CHECK (tipo IN ('Gasto', 'Inversión', 'Fondo')),
-    es_acumulativa BOOLEAN DEFAULT FALSE,
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR(20) NOT NULL
+    is_cumulative BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 3. Tabla de Presupuestos (Budgets)
--- Define cuánto planeas asignar a una categoría en un mes/año específico
+-- 2. Tabla de Presupuestos (Budgets)
+-- Define cuánto se planeas asignar a una categoría en un mes/año específico
 CREATE TABLE IF NOT EXISTS budgets (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
-    mes INTEGER NOT NULL CHECK (mes BETWEEN 1 AND 12),
-    anio INTEGER NOT NULL,
-    monto_asignado DECIMAL(15, 2) DEFAULT 0.00,
-    UNIQUE (user_id, category_id, mes, anio) -- Evita duplicados para el mismo mes/cat
+    amount DECIMAL(15, 2) DEFAULT 0.00,
+    budget_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    UNIQUE (id, category_id)
 );
 
--- 4. Tabla de Transacciones (La realidad)
+-- 3. Tabla de Transacciones (La realidad)
 CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL,
     monto DECIMAL(15, 2) NOT NULL,
-    fecha DATE NOT NULL DEFAULT CURRENT_DATE,
     descripcion TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 5. Tabla de Balances Mensuales (MonthlyBalances)
+-- 4. Tabla de Balances Mensuales (MonthlyBalances)
 -- Histórico para el sistema de "Sinking Funds" (Acumulados)
 CREATE TABLE IF NOT EXISTS monthly_balances (
     id SERIAL PRIMARY KEY,
     category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
-    mes INTEGER NOT NULL CHECK (mes BETWEEN 1 AND 12),
-    anio INTEGER NOT NULL,
-    saldo_inicial DECIMAL(15, 2) DEFAULT 0.00, -- Lo que sobró del mes anterior
-    saldo_final DECIMAL(15, 2) DEFAULT 0.00,   -- (Inicial + Presupuesto - Gastos)
-    UNIQUE (category_id, mes, anio)
+    start_balance DECIMAL(15, 2) DEFAULT 0.00, -- Lo que sobró del mes anterior
+    final_balance DECIMAL(15, 2) DEFAULT 0.00,   -- (Inicial + Presupuesto - Gastos)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    UNIQUE (id, category_id)
 );
