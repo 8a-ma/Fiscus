fcc0c45862d741cca80c58777d3bb36bf95d4fd1
8a-ma <e0as0k98x001b@gmail.com>
2026-02-08 11:46:53 -0300

    Change names enviroments

--- api/src/api.py
+++ api/src/api.py
@@ -1,12 +1,10 @@
 import secrets
 import logging
 from flask import Flask, Response
-from flask_wtf import CSRFProtect
 from src.config.config import Config
 from src.services.factory import services
 
 app = Flask(__name__)
-csrf = CSRFProtect()
 logger = Config.get_logger(__name__) or logging.getLogger()
 
 app.config.update(
@@ -20,8 +18,4 @@ def health():
     return Response(status=204)
 
 if __name__ == "__main__":
-    try:
-        app.run(host="0.0.0.0", port=8000)
-
-    finally:
-        pass
+    app.run(host="0.0.0.0", port=8000)
